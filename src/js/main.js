e571870a45a6d999361fa78a2bdcf626b8d1a331
Filipe Bojikian Rissi <f.b.rissi@gmail.com>
2019-02-20 16:49:44 -0300

    Simplified icons logic. Logic for extraction data from the icon removed from the code and included in the readme.

--- src/js/main.js
+++ src/js/main.js
@@ -5,13 +5,11 @@ import ItemLink from './components/ItemLink.vue';
 import ItemBirthday from './components/ItemBirthday.vue';
 import VModal from 'vue-js-modal';
 import IconSelect from './components/IconSelect.vue';
-import VSelect from 'vue-select';
 
 Vue.component('sections', Section);
 Vue.component('item-link', ItemLink);
 Vue.component('item-birthday', ItemBirthday);
 Vue.component('icon-select', IconSelect);
-Vue.component('v-select', VSelect);
 
 Vue.use(VModal);
 
