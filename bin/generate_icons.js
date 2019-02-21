18d496117653055928f732013a9d133ba285f5e8
Filipe Bojikian Rissi <f.b.rissi@gmail.com>
2019-02-21 12:01:19 -0300

    Using fortawesome to not need web scraping.

--- bin/generate_icons.js
+++ bin/generate_icons.js
@@ -2,64 +2,14 @@
 /* eslint-disable no-console */
 const {resolve} = require('path');
 const baseDir = resolve(__dirname, '../');
-const axios = require('axios');
-const cheerio = require('cheerio');
 const {writeFileSync} = require('fs');
-const Horseman = require('node-horseman');
+const {fas} = require('@fortawesome/free-solid-svg-icons');
+const {far} = require('@fortawesome/free-regular-svg-icons');
+const {fab} = require('@fortawesome/free-brands-svg-icons');
 
-const getFontawesome = async () => {
-    try {
+const json = Object.values(fas)
+    .concat(Object.values(far))
+    .concat(Object.values(fab))
+    .map(a => a.prefix + " fa-" + a.iconName);
 
-        return await axios.get('https://use.fontawesome.com/releases/v5.2.0/css/all.css');
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-const horseman = new Horseman({
-    injectJquery: true,
-    ignoreSSLErrors: true,
-    webSecurity: false,
-    loadImages: false,
-});
-
-horseman
-    .open('https://fontawesome.com/cheatsheet#brands')
-    .html('body')
-    .then(async (html) => {
-        try {
-            const $ = cheerio.load(html);
-            const fontawesome = await getFontawesome();
-            const incons = [];
-            const json = [];
-
-            $('#solid .icon-name')
-                .each(function () {
-                    incons.push('fas fa-' + $(this).text());
-                });
-            $('#regular .icon-name')
-                .each(function () {
-                    incons.push('far fa-' + $(this).text());
-                });
-            $('#brands .icon-name')
-                .each(function () {
-                    incons.push('fab fa-' + $(this).text());
-                });
-
-            fontawesome.data
-                .match(/(?!\.)fa-[-\w]+(?=:before)/g)
-                .forEach((element) => {
-                    ['fas', 'far', 'fab'].forEach(prefix => {
-                        let item = prefix + ' ' + element;
-                        if (!json.includes(item) && incons.includes(item)) {
-                            json.push(item);
-                        }
-                    });
-                });
-
-            writeFileSync(`${baseDir}/src/js/components/icons.json`, JSON.stringify(json));
-        } catch (error) {
-            console.error(error);
-        }
-    })
-    .close();
+writeFileSync(`${baseDir}/src/js/components/icons.json`, JSON.stringify(json));
